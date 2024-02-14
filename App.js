06e8220fe6c8f0ed9b4eb9650e712e62658f5e32
DESKTOP-CCDS2EU\natas <natasha_sheko@yahoo.com>
2024-02-14 22:48:00 +0100

    FlatList

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
+    const [enteredGoalText, setEnteredGoalText] = useState('');
+    const [courseGoals, setCourseGoals] = useState([]);
+
+    const styles = StyleSheet.create({
+        appContainer: {
+            paddingTop:50,
+            paddingHorizontal:16,
+            flex: 1,
+        },
+        inputContainer: {
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginBottom: 24,
+            borderBottomColor: '#cccccc',
+            borderBottomWidth: 1,
+            flex:1,
+        },
+        textInput: {
+            borderWidth: 1,
+            borderColor: '#cccccc',
+            width: '70%',
+            marginRight: 8,
+            padding: 8,
+        },
+        goalsContainer: {
+            flex: 5,
+        },
+        goalItem: {
+            borderWidth: 1,
+            borderRadius: 5,
+            borderColor: '#4e0fcc',
+            backgroundColor:'#5e0ecc',
+            margin: 8,
+            padding: 8
+        },
+        goalText: {
+            color: 'white'
+        }
+    });
+
+    const goalInputHandler = (enteredText) => {
+        setEnteredGoalText(enteredText);
+        return null;
+    }
+
+    const addGoalHandler = () => {
+        setCourseGoals(currentCourseGoals => [...currentCourseGoals, enteredGoalText]);
+    }
   return (
-    <View style={styles.container}>
-      <Text>Nataliya Wierts App</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.appContainer}>
+          <View style={styles.inputContainer}>
+                <TextInput placeholder='Your course goal' style={styles.textInput} onChangeText={goalInputHandler}/>
+                <Button title='Add goal' onPress={addGoalHandler}/>
+          </View>
+          <View style={styles.goalsContainer}>
+              <FlatList data={courseGoals} renderItem={(itemData ) => {return (
+                  <View key ={itemData.item} style={styles.goalItem}>
+                      <Text style={styles.goalText}>
+                          {itemData.item}
+                      </Text>
+                  </View>);
+              }}/>
+          </View>
+      </View>
   );
+
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
